7b96eb563fc1375e2e08d5fbb5dd039a8e4fda1e
Jenil133 <jenilsavalia058@gmail.com>
2021-06-11 10:24:15 +0530

    changes o n trial-1

--- Jack.py
+++ Jack.py
@@ -1,6 +1,17 @@
 
 #this is line 1
 print('hellow world')
+#this is line 1
+print('hellow world')
+
+
+
+#this is line 1
+print('hellow world')
+#this is line 4rjhghdzkgngaeohnksv;ozehgeodxkn
+print('hellow world')
+
+
 
 
 
